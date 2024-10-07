10a0b480dc84884fce52a53c98b737348a95af2b
Nishan Thapa <tnishan110@gmail.com>
2024-10-07 12:46:44 +0545

    State initialized, global context

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,23 +1,38 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 const IncomeExpenses = () => {
-  return (
-    <div className='inc-exp-container'>
-        <div>
-            <h4>Income</h4>
-            <p className='money plus'>
-            +$0.00
-            </p>
-        </div>
-        <div>
-            <h4>Expense</h4>
-            <p className='money minus'>
-            -$0.00
-            </p>
-        </div>
-      
-    </div>
-  )
-}
+  const { transactions } = useContext(GlobalContext); // Destructure transactions from context
 
-export default IncomeExpenses
+  // Ensure transactions is an array
+  if (!Array.isArray(transactions)) {
+    return <div>No transactions available</div>;
+  }
+
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense = amounts
+    .filter(item => item < 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  return (
+    <div className="inc-exp-container">
+      <div>
+        <h4>Income</h4>
+        <p className="money plus">+${income}</p>
+      </div>
+      <div>
+        <h4>Expense</h4>
+        <p className="money minus">-${Math.abs(expense)}</p>
+      </div>
+    </div>
+  );
+};
+
+export default IncomeExpenses;
